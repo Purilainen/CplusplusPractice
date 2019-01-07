e7cb21a9f7dafdabed4a0ff4a9aa764a8841b60c
Purilainen <p.kosola@gmail.com>
2019-01-07 17:43:33 +0200

    Custom Exceptions (Error handling)

---
+++ StandardExceptions/CustomExceptions.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <exception>
+#include <Windows.h>
+
+class CustomException : public std::exception
+{
+public:
+	virtual const char* what() const throw()
+	{
+		return "Something went wrong!";
+	}
+};
+
+class Test
+{
+public:
+	void goesWrong()
+	{
+		throw CustomException();
+	}	
+};
+
+
+int main()
+{
+	Test test;
+
+	try
+	{
+		test.goesWrong();
+	}
+	catch (CustomException &e)
+	{
+		std::cout << "Caught exception: " << e.what() << std::endl;
+	}
+
+
+	for (;;)
+	{
+		if (GetAsyncKeyState(VK_ESCAPE))
+			return 0;
+	}
+}
